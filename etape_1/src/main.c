49009999da5d57bbcda8b0eb790c9b617ee4ae16
guerot_a <anthony.guerot@gmail.com>
2014-03-07 21:10:39 +0100

    create dirs for each exercices

--- etape_1/src/main.c
+++ etape_1/src/main.c
@@ -5,14 +5,13 @@
 ** Login   <guerot_a@epitech.net>
 **
 ** Started on  Fri Mar  7 20:33:05 2014 guerot_a
-** Last update Fri Mar  7 20:47:07 2014 guerot_a
+** Last update Fri Mar  7 21:08:37 2014 guerot_a
 */
 
-#include <SDL/SDL.h>
+#include <stdio.h>
 
 int	main(int ac, char** av)
 {
-  SDL_Init(SDL_INIT_VIDEO);
-
-  return (0);
+  if (ac > 2)
+    epikong()
 }
