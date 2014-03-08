db2c74e6a39959130e58620ff85a004812135d2c
sadows_g <sadows_g@epitech.eu>
2014-03-08 20:41:32 +0100

    toto

--- etape_2/src/manage_ia.c
+++ etape_2/src/manage_ia.c
@@ -5,7 +5,7 @@
 ** Login   <guerot_a@epitech.net>
 **
 ** Started on  Sat Mar  8 15:07:11 2014 guerot_a
-** Last update Sat Mar  8 20:25:57 2014 SADOWSKI Geoffroy
+** Last update Sat Mar  8 20:40:57 2014 SADOWSKI Geoffroy
 */
 
 #include "epikong.h"
@@ -26,7 +26,7 @@ void    bot_left(t_map* map, t_monster *data)
 
   x = data->pos_x;
   y = data->pos_y;
-  if (!bot_can_walk(map, x-1, y) || map->data[y][x - 1] == 'w')
+  if (!bot_can_walk(map, x-1, y) || map->data[y][x - 1] == 'w' || map->data[y][x - 1] == 'm')
     {
       data->direction = DIR_RIGHT;
       return;
@@ -42,7 +42,7 @@ void    bot_right(t_map* map, t_monster *data)
 
   x = data->pos_x;
   y = data->pos_y;
-  if (!bot_can_walk(map, x+1, y) || map->data[y][x + 1] == 'w')
+  if (!bot_can_walk(map, x+1, y) || map->data[y][x + 1] == 'w' || map->data[y][x - 1] == 'm')
     {
       data->direction = DIR_LEFT;
       return;
