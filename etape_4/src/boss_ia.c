0ef2cce86b4f12c5b8b9fe5ebb4961f753a162b1
guerot_a <anthony.guerot@gmail.com>
2014-03-09 21:21:19 +0100

    .

---
+++ etape_4/src/boss_ia.c
@@ -0,0 +1,15 @@
+/*
+** boss_ia.c for  in /home/guerot_a/depots/T2Rush1/etape_4
+**
+** Made by guerot_a
+** Login   <guerot_a@epitech.net>
+**
+** Started on  Sun Mar  9 21:15:16 2014 guerot_a
+** Last update Sun Mar  9 21:16:10 2014 guerot_a
+*/
+
+#include "epikong.h"
+
+int	run_boss_ia(t_map* map, t_objlist* objlist)
+{
+}
