8f564754eaa90fddc611829340dcaf2e42f85ea7
genes_k <genes_k@epitech.eu>
2014-03-09 21:18:25 +0100

    .

--- etape_2/src/menu.c
+++ etape_2/src/menu.c
@@ -5,14 +5,14 @@
 ** Login   <sadows_g@epitech.net>
 **
 ** Started on  Sun Mar  9 00:52:42 2014 SADOWSKI Geoffroy
-** Last update Sun Mar  9 17:02:23 2014 genes_k
+** Last update Sun Mar  9 20:41:46 2014 genes_k
 */
 
 #include "epikong.h"
 
 const char*	stages[5] =
   {
-    "map/stage9.map",
+    "map/boss.map",
     "map/stage16.map",
     "map/stage12.map",
     "map/stage14.map",
